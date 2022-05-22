e26837885b31db5cb6e001911838d0ea90e804eb
Douglas Miller <durgadas311@gmail.com>
2022-05-21 20:40:46 -0500

    programs: fix recurs.s example

--- hw2000/programs/src/recurs.s
+++ hw2000/programs/src/recurs.s
@@ -25,24 +25,46 @@
 main:
 	cam	060
 	lca	_stk,x1		// setup stack
-	lca	x1,x2		// init frame pointer
 	bs	_c4,x1		// alloc local variable 'level'
-	lca	_c0,-4(x2)	// init level=0
+	lca	x1,x2		// init frame pointer
+//
+//       +-------------+
+// (+8)  |     ---     | (+8)
+//       +-------------+
+// (+4)  |     ---     | (+4)
+//       +-------------+
+// x1 -> |   * level * | <- x2 (frame ptr)
+//       +-------------+
+// (-4)  |             | (-4)
+//       +-------------+
+	lca	_c0,0(x2)	// init level=0
 	pdt	_m1,012,07,0	// print function name
 	pcb	.,012,07,010
 	pdt	_m2,012,07,01	// print start message
 	pcb	.,012,07,010
 	// callee can alter 'level' unless we copy it
-	lca	-4(x2),-4(x1)	// push copy of 'level' on stack
+	lca	0(x2),-4(x1)	// push copy of 'level' on stack
 	bs	_c4,x1		// (adjust for push)
+//       +-------------+
+// (+12) |     ---     | (+8)
+//       +-------------+
+// (+8)  |     ---     | (+4)
+//       +-------------+
+// (+4)  |   * level * | <- x2 (frame ptr)
+//       +-------------+
+// x1 -> |     arg     | (-4)
+//       +-------------+
 	b	recurs		// call recurs(level)
 	ba	_c4,x1		// pop/discard 'level' copy
 	pdt	_m1,012,07,0	// print function name
 	pcb	.,012,07,010
 	pdt	_m3,012,07,01	// print end message
 	pcb	.,012,07,010
-//	b	130		// return to MOD1 ("exit")
+	.admode	3
+	cam	040
+	b	(139)		// return to monitor
 	h	.
+	.admode	4
 
 // void recurs(int l) {
 //	int lev = l + 1;
@@ -51,29 +73,47 @@ main:
 //	printf("recurse level %d exit\n", lev);
 // }
 recurs:
+//			 <- x2
+//       +-------------+
+// x1 -> |     arg     |
+//       +-------------+
+// (-4)  |     ---     |
+//       +-------------+
+// (-8)  |     ---     |
+//       +-------------+
+// (-12) |     ---     |
+//       +-------------+
 ///////////////////////
 // function preamble:
 	scr	-4(x1),070	// save return address
 	lca	x2,-8(x1)	// save frame pointer
-	bs	_c8,x1		// adjust stack for function
 	lca	x1,x2		// set new frame pointer
-	bs	_c4,x1		// alloc local vars
+	bs	_c12,x1		// adjust stack for function
+//       +-------------+
+// (+12) |     arg     | <- x2
+//       +-------------+
+// (+8)  |   return    | (-4)
+//       +-------------+
+// (+4)  |     x2      | (-8)
+//       +-------------+
+// x1 -> |     lev     | (-12)
+//       +-------------+
 ///////////////////////
 // function body:
-	lca	8(x2),-4(x2)	// copy param to local var
-	ba	_c1,-4(x2)	// increment level
-	exm	-4(x2),_r1+14,001 // put level (digit) in message
+	lca	4(x2),-12(x2)	// copy param to local var
+	ba	_c1,-12(x2)	// increment level
+	exm	-12(x2),_r1+14,001 // put level (digit) in message
 	pdt	_r1,012,07,0	// print...
 	pcb	.,012,07,010	//
 	pdt	_r2,012,07,01	// print rest of message
 	pcb	.,012,07,010	//
-	bce	1f,-4(x2),05	// stop recursion at 5
+	bce	1f,-12(x2),05	// stop recursion at 5
 	// callee can modify level unless we copy
-	lca	-4(x2),-4(x1)	// push 'lev' on stack
+	lca	-12(x2),-4(x1)	// push 'lev' on stack
 	bs	_c4,x1		// (adjust for push)
 	b	recurs		// call recurs(lev)
 	ba	_c4,x1		// discard func param
-1:	exm	-4(x2),_r1+14,001 // put level in message
+1:	exm	-12(x2),_r1+14,001 // put level in message
 	pdt	_r1,012,07,0	// print...
 	pcb	.,012,07,010	//
 	pdt	_r3,012,07,01	// print rest of message
@@ -105,3 +145,4 @@ _c0:	.bin	0#4	// init for params, addrs
 _c1:	.bin	1#1
 _c4:	.bin	4#1
 _c8:	.bin	8#1
+_c12:	.bin	12#1
